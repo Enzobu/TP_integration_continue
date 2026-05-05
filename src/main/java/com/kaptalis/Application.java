5e0eb7fe7c4352083237979e1601bd9c0ecc447b
Enzo Palermo <palermo.enzo.ep@gmail.com>
2026-05-05 16:28:01 +0200

    feat: Update

--- src/main/java/com/kaptalis/Application.java
+++ src/main/java/com/kaptalis/Application.java
@@ -1,11 +1,18 @@
 package com.kaptalis;
 
+import java.util.logging.Logger;
+
 public class Application {
 
-    public Application() {}
+    private static final Logger logger = Logger.getLogger("com.kaptalis");
+
+    private Application() {
+        // empêche l'instanciation
+    }
 
     public static void main(String[] args) {
         System.out.println("Kaptalis Monitoring System - Active");
+        logger.fine("Kaptalis Monitoring System - Active");
     }
 
     public static boolean isTemperatureValid(double temp) {
