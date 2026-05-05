b1ea6d33032e1681ca55ee590ca0adbe7f3c6bb0
Enzo Palermo <palermo.enzo.ep@gmail.com>
2026-05-05 15:45:52 +0200

    feat: Update

--- src/main/java/com/kaptalis/App.java
+++ src/main/java/com/kaptalis/App.java
@@ -1,13 +1,16 @@
 package com.kaptalis;
 
-public class App {  
+public class App {
+    String unusedVariable = "Audit_MedTech";
 
        public static void main(String[] args) {
         System.out.println("Kaptalis Monitoring System - Active");
     }
 
     public static boolean isTemperatureValid(double temp) {
-        return (temp >= 2.0 && temp <= 8.0);
+        if (temp > 100) {}
+
+        return temp >= 2.0 && temp <= 8.0;
     }
 }
 
